9692b757b21b7f7b1857e78f6cc6f47430097d17
OneAndonlyFinbar <finbar@finbar.xyz>
2021-12-19 22:01:50 -0500

    Update updater.mjs

--- updater.mjs
+++ updater.mjs
@@ -3,31 +3,18 @@ import fetch from 'node-fetch'
 const config = JSON.parse(fs.readFileSync('./updaterConfig.json', 'utf-8'))
 
 async function updateBot() {
-    if (config.PRIVATE_REPO == "y") {
-        var headers = {"Authorization": `Token ${config.GITHUB_AUTH_TOKEN}`}
-        console.log("Updating Commands")
-        var commands = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers})
-        var commandsText = await commands.text()
-        fs.writeFileSync('.\\data\\commands.json', commandsText)
-        console.log("Commands Updated")
-        console.log("Updating Events")
-        var events = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers})
-        var eventsText = await events.text()
-        fs.writeFileSync('.\\data\\events.json', eventsText)
-        console.log("Events Updated")
-    }
-    else {
-        console.log("Updating Commands")
-        var commands = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
-        var commandsText = await commands.text()
-        fs.writeFileSync('.\\data\\commands.json', commandsText)
-        console.log("Commands Updated")
-        console.log("Updating Events")
-        var events = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`)
-        var eventsText = await events.text()
-        fs.writeFileSync('.\\data\\events.json', eventsText)
-        console.log("Events Updated")
-    }
+    if(typeof config.PRIVATE_REPO !== 'boolean') return console.log('Invalid PRIVATE_REPO in updaterConfig.json')
+    const headers = {"Authorization": `Token ${config.GITHUB_AUTH_TOKEN}`}
+    console.log("Updating Commands")
+    const commands = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`);
+    const commandsText = await commands.text()
+    fs.writeFileSync('.\\data\\commands.json', commandsText)
+    console.log("Commands Updated")
+    console.log("Updating Events")
+    const events = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`);
+    const eventsText = await events.text()
+    fs.writeFileSync('.\\data\\events.json', eventsText)
+    console.log("Events Updated")
 }
 
 (async function start() {
