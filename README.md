b6f2396392572451d522a02d4cb9abf0f69cbef2
Fisheiyy <myallneedsemail@email.com>
2021-12-19 19:36:35 -0600

    working no commit bullshit updater

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # dbm-discord-bot-updater
 dbm-discord-bot-updater requires Node 16 per DBM v2 and it is recommended to use the [latest version of Node 16](https://nodejs.org/en/)
-to use this you must copy updater.mjs to your bots directory and use [this event](https://rawdata.dbm-network.org/commands?show=MkHghJ&page=1)
+
+# installation
+To Install this you must copy updater.mjs and updaterConfig.json to your bots directory, But you must also edit the config.
+1. If your bots Github Repository is private put "y" in the empty "" next to "PRIVATE_REPO" else put "n"
+2. Put your Github Username in the empty "" next to "GITHUB_USERNAME"
+3. Put your Github Repository Name in the empty "" next to "GITHUB_REPO_NAME"
+4. If your Github Reposiory IS private put your Github Authorization Key in the empty "" next to "GITHUB_AUTH_TOKEN" else leave the "" empty (you can find your github authorization key under setting > developer setting > personal access tokens, if you do not already have one click "Generate new token" and input all the details neccessary)
--- updater.mjs
+++ updater.mjs
@@ -1,51 +1,35 @@
 import fs from 'fs-extra'
 import fetch from 'node-fetch'
-const allCommits = ".\\commits.json"
-const currentCommit = ".\\currentCommit.json"
-
-if (allCommits.indexOf(currentCommit.sha) > 1) {
-    var commitsSince = allCommits.indexOf(currentCommit.sha)
-    if (commitsSince == 1) {
-        console.log(`Updating, there has been ${commitsSince} commit since last update`)
-    }
-    if (commitsSince != 1) {
-        console.log(`Updating, there has been ${commitsSince} commits since last update`)
-    }
-    await updateBot()
-    await updateCommits()
-}
+const config = JSON.parse(fs.readFileSync('.\\updaterConfig.json', 'utf-8'))
 
 async function updateBot() {
-    console.log("Updating Commands")
-    var commands = await fetch('your commands.json raw github url')
-    var commandsText = await commands.text()
-    fs.writeFile('.\\data\\commands.json', commandsText, (err) => {
-        if (err) {return console.log(err)}
-    })
-	console.log("Commands Updated")
-    console.log("Updating Events")
-    var events = await fetch('your events.json raw github url')
-    var eventsText = await events.text()
-    fs.writeFile('.\\data\\events.json', eventsText, (err) => {
-        if (err) {return console.log(err)}
-    })
-	console.log("Events Updated")
+    if (config.PRIVATE_REPO == "y") {
+        var headers = {"Authorization": `Token ${config.GITHUB_AUTH_TOKEN}`}
+        console.log("Updating Commands")
+        var commands = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers})
+        var commandsText = await commands.text()
+        fs.writeFileSync('.\\data\\commands.json', commandsText)
+        console.log("Commands Updated")
+        console.log("Updating Events")
+        var events = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers})
+        var eventsText = await events.text()
+        fs.writeFileSync('.\\data\\events.json', eventsText)
+        console.log("Events Updated")
+    }
+    else {
+        console.log("Updating Commands")
+        var commands = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
+        var commandsText = await commands.text()
+        fs.writeFileSync('.\\data\\commands.json', commandsText)
+        console.log("Commands Updated")
+        console.log("Updating Events")
+        var events = await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`)
+        var eventsText = await events.text()
+        fs.writeFileSync('.\\data\\events.json', eventsText)
+        console.log("Events Updated")
+    }
 }
 
-async function updateCommits() {
-    console.log("Getting Commits")
-    // var headers = {"Authorization": "Token yourpersonalaccesstoken"}
-    // var commits = await fetch('your bot repo url', {"method": "GET", "headers": headers})
-    // if your repo is public you can comment the line below and uncomment the 2 lines above
-    var commits = await fetch('your bot repo url')
-    var commitsJSON = await commits.json()
-    var commitsJSON = JSON.stringify(commitsJSON)
-    console.log("Updating All Commits")
-    fs.writeFile(allCommits, commitsJSON, (err) => {
-        if (err) {return console.log(err)}
-    })
-    console.log("Updating Current Commit")
-    fs.writeFile(currentCommit, commitsJSON[0], (err) => {
-        if (err) {return console.log(err)}
-    })
-}
+(async function start() {
+    await updateBot()
+}())
