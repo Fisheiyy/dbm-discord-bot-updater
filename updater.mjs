cddd8192f70f8b41e5bb6c0861c722c98332bae2
Fisheiyy <myallneedsemail@email.com>
2021-12-21 01:10:51 -0600

    Update updater.mjs

--- updater.mjs
+++ updater.mjs
@@ -3,38 +3,33 @@ import fetch from 'node-fetch'
 const config = JSON.parse(fs.readFileSync('./updaterConfig.json', 'utf-8'))
 
 async function configValidator() {
-    if (config.GITHUB_USERNAME == "") {
-        console.log("GITHUB_USERNAME is not set in updaterConfig.json")
-        process.exit()
+    if (config.GITHUB_USERNAME !== "") {
+        if (config.GITHUB_REPO_NAME !== "") {
+            if (typeof config.PRIVATE_REPO == 'boolean') {
+                if (config.PRIVATE_REPO == true && config.GITHUB_AUTH_TOKEN == "") {throw new Error("GITHUB_AUTH_TOKEN is not set in updaterConfig.json")}
+                else {console.log("Private Repo = False")}
+            }
+            else {throw new Error("PRIVATE_REPO is not a valid boolean in updaterConfig.json")}
+        }
+        else {throw new Error("GITHUB_REPO_NAME is not set in updaterConfig.json")}
     }
-    if (config.GITHUB_REPO_NAME == "") {
-        console.log("GITHUB_REPO_NAME is not set in updaterConfig.json")
-        process.exit()
-    }
-    if (typeof config.PRIVATE_REPO !== 'boolean') {
-        console.log("PRIVATE_REPO is not a valid boolean in updaterConfig.json")
-        process.exit()
-    }
-    if (config.PRIVATE_REPO == true && config.GITHUB_AUTH_TOKEN == "") {
-        console.log("GITHUB_AUTH_TOKEN is not set in updaterConfig.json")
-        process.exit()
-    }
-    var githubApi = await fetch(`https://api.github.com/`)
-    var githubCom = await fetch(`https://github.com/`)
-    if (githubApi && githubCom != 200) {throw new Error("Github is having issues, try again later")}
-    console.log("Config Valid and Github OK")
+    else {throw new Error("GITHUB_USERNAME is not set in updaterConfig.json")}
+    const githubApi = await fetch(`https://api.github.com/`)
+    const githubCom = await fetch(`https://github.com/`)
+    if (githubApi.status && githubCom.status != 200) {throw new Error("Github is having issues, try again later")}
+    console.log("Config Valid and Github Reponse OK")
 }
 
 async function updateBot() {
-    var headers = {"Authorization": `Token ${config.GITHUB_AUTH_TOKEN}`}
+    const headers = {"Authorization": `Token ${config.GITHUB_AUTH_TOKEN}`}
     console.log("Updating Commands")
-    var commands = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
-    var commandsText = await commands.text()
+    const commands = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
+    const commandsText = await commands.text()
     fs.writeFileSync('.\\data\\commands.json', commandsText)
     console.log("Commands Updated")
     console.log("Updating Events")
-    var events = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`)
-    var eventsText = await events.text()
+    const events = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`)
+    const eventsText = await events.text()
     fs.writeFileSync('.\\data\\events.json', eventsText)
     console.log("Events Updated")
 }
