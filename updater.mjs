2cda56010ce8a34eb33e138cad90b10afe91b2e2
Fisheiyy <myallneedsemail@email.com>
2021-12-21 02:58:15 -0600

    Update updater.mjs

--- updater.mjs
+++ updater.mjs
@@ -18,16 +18,14 @@ async function configValidator() {
     const githubApi = await fetch(`https://api.github.com/`)
     const githubCom = await fetch(`https://github.com/`)
     if (githubApi.status && githubCom.status != 200) {throw new Error("Github is having issues, try again later")}
-    console.log("Config Valid and Github Reponse OK")
+    console.log("Config Valid and Github Response OK")
 }
 
 async function updateCommands() {
-    console.log("Updating Commands")
     const commands = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
     const commandsText = await commands.text()
     fs.writeFileSync('.\\data\\commands.json', commandsText)
     console.log("Commands Updated")
-    console.log("Updating Events")
     const events = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/events.json`)
     const eventsText = await events.text()
     fs.writeFileSync('.\\data\\events.json', eventsText)
@@ -35,7 +33,6 @@ async function updateCommands() {
 }
 
 async function updateSettings() {
-    console.log("Updating Settings")
     const settings = config.PRIVATE_REPO ? await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/settings.json`, {"method": "GET", "headers": headers}) : await fetch(`https://raw.githubusercontent.com/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/main/data/commands.json`)
     const settingsText = await settings.text()
     fs.writeFileSync('.\\data\\settings.json', settingsText)
