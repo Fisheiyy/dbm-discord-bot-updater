1666a2fedde1665cd552d77e45914ef6edbe5254
Fisheiyy <myallneedsemail@email.com>
2021-12-21 20:41:31 -0600

    multifunctionality
    can now update actions, settings.json and resources folder

--- updater.mjs
+++ updater.mjs
@@ -39,9 +39,55 @@ async function updateSettings() {
     console.log("Settings Updated")
 }
 
-// async function updateResources() {}
+async function updateResources() {
+    const resources = config.PRIVATE_REPO ? await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/resources`, {"method": "GET", "headers": headers}) : await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/resources`)
+    const resourcesJSON = await resources.json()
+    var i = 0
+    while (i < resourcesJSON.length) {
+        var resourceContent = config.PRIVATE_REPO ? await fetch(`${resourcesJSON[i].download_url}`, {"method": "GET", "headers": headers}) : await fetch(`${resourcesJSON[i].download_url}`)
+        var resourceText = await resourceContent.text()
+        fs.writeFileSync('.\\resources\\' + resourcesJSON[i].name, resourceText)
+        console.log("Resources Downloaded: " + resourcesJSON[i].name)
+        i++
+    }
+}
 
-// async function updateActions() {}
+async function updateActions() {
+    const actions = config.PRIVATE_REPO ? await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/actions`, {"method": "GET", "headers": headers}) : await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/actions`)
+    const actionsJSON = await actions.json()
+    const events = config.PRIVATE_REPO ? await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/events`, {"method": "GET", "headers": headers}) : await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/events`)
+    const eventsJSON = await events.json()
+    const extensions = config.PRIVATE_REPO ? await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/extensions`, {"method": "GET", "headers": headers}) : await fetch(`https://api.github.com/repos/${config.GITHUB_USERNAME}/${config.GITHUB_REPO_NAME}/contents/extensions`)
+    const extensionsJSON = await extensions.json()
+    var i = 0
+    while (i < actionsJSON.length) {
+        var actionContent = config.PRIVATE_REPO ? await fetch(`${actionsJSON[i].download_url}`, {"method": "GET", "headers": headers}) : await fetch(`${actionsJSON[i].download_url}`)
+        var actionText = await actionContent.text()
+        fs.writeFileSync('.\\actions\\' + actionsJSON[i].name, actionText)
+        i++
+    }
+    console.log("Actions Updated")
+    var i = 0
+    while (i < eventsJSON.length) {
+        var eventContent = config.PRIVATE_REPO ? await fetch(`${eventsJSON[i].download_url}`, {"method": "GET", "headers": headers}) : await fetch(`${eventsJSON[i].download_url}`)
+        var eventText = await eventContent.text()
+        fs.writeFileSync('.\\events\\' + eventsJSON[i].name, eventText)
+        i++
+    }
+    console.log("Events Updated")
+    var i = 0
+    while (i < extensionsJSON.length) {
+        if (extensionsJSON[i].download_url == null) {
+            i++
+            continue
+        }
+        var extensionContent = config.PRIVATE_REPO ? await fetch(`${extensionsJSON[i].download_url}`, {"method": "GET", "headers": headers}) : await fetch(`${extensionsJSON[i].download_url}`)
+        var extensionText = await extensionContent.text()
+        fs.writeFileSync('.\\extensions\\' + extensionsJSON[i].name, extensionText)
+        i++
+    }
+    console.log("Extensions Updated")
+}
 
 (async function start() {
     await configValidator()
@@ -54,12 +100,14 @@ async function updateSettings() {
             case "settings":
                 await updateSettings()
                 break
-            // case "resources":
-            //     await updateResources()
-            //     break
-            // case "actions":
-            //     await updateActions()
-            //     break
+            case "resources":
+                console.log("Updating Resources, this may take a while")
+                await updateResources()
+                break
+            case "actions":
+                console.log("Updating Actions, this may take a while")
+                await updateActions()
+                break
             default:
                 throw new Error("Invalid Argument")
         }
